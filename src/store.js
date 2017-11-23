7491fb1c0a787c5540086965edeab8c6a1697ec2
neira1991 <stevencali9113@gmail.com>
2017-11-23 15:52:23 -0500

    implementando combine reducers

--- src/store.js
+++ src/store.js
@@ -1,35 +1,32 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk'
 
-const reducer = ( state, action ) => {
+const products = ( state=[], action ) => {
   switch (action.type) {
     case 'REPLACE_PRODUCTS':
-      return {
-        ...state,
-        products: action.products
-      };
-    case 'ADD_TO_CART':
-    console.log('ADD_TO_CART',state);
-      return {
-        ...state,
-        cart : state.cart.concat(action.product).filter(element => element !== undefined)
-      }
-    case 'REMODE_FROM_CAR':
-    console.log('REMODE_FROM_CAR',state);
-      return {
-        ...state,
-        cart: state.cart.filter(product => product.id !== action.product.id)
-      }
+      return action.products
     default:
   }
   return state;
 };
 
+const cart = ( state=[], action ) => {
+  switch (action.type) {
+    case 'ADD_TO_CART':
+    console.log('ADD_TO_CART',state);
+      return state.concat(action.product).filter(element => element !== undefined)
+    case 'REMODE_FROM_CAR':
+    console.log('REMODE_FROM_CAR',state);
+      return state.filter(product => product.id !== action.product.id)
+    default:
+  }
+  return state;
+};
 const logger = store => next => action => {
   let result;
   result = next(action)
-   console.log('next state', store.getState())
-   return result
+  console.log('next state', store.getState())
+  return result
 }
 
-export default createStore(reducer, { cart: [], product: [],products: []}, applyMiddleware(logger, thunk));
+export default createStore(combineReducers({ cart, products}), applyMiddleware(logger, thunk));
