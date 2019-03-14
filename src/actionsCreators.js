82194e4590286a1ff32ef07c85919a9b4100c493
neira1991 <stevencali9113@gmail.com>
2019-03-14 15:47:58 +0100

    create file reducers

--- src/actionsCreators.js
+++ src/actionsCreators.js
@@ -1,57 +1,44 @@
 const loadProducts = () => {
-  return dispatch => {
-    return fetch('http://localhost:3001/productsneira')
-    .then(response => response.ok ? response.json() : null)
-    .then(resposeData => {
-      dispatch({
-        type: "REPLACE_PRODUCTS",
-        products: resposeData
-      })
-      console.log(resposeData);
-    });
-
+  return {
+    type: 'REPLACE_PRODUCTS',
+    products: [
+      {
+        id: 1,
+        name: 'Hipster Ultimate',
+        price: 299,
+        image:
+          'https://s3.amazonaws.com/makeitreal/projects/e-commerce/camiseta-1.jpg'
+      },
+      {
+        id: 2,
+        name: 'On Motion Live',
+        price: 99,
+        image:
+          'https://s3.amazonaws.com/makeitreal/projects/e-commerce/camiseta-2.jpg'
+      },
+      {
+        id: 3,
+        name: 'Underground Max',
+        price: 149,
+        image:
+          'https://s3.amazonaws.com/makeitreal/projects/e-commerce/camiseta-3.jpg'
+      }
+    ]
   }
+}
 
-  // return (
-  //   {
-  //     type: "REPLACE_PRODUCTS",
-  //     products: [
-  //       {
-  //         "id": 1,
-  //         "name": "Hipster Ultimate",
-  //         "price": 299,
-  //         "image": "https://s3.amazonaws.com/makeitreal/projects/e-commerce/camiseta-1.jpg"
-  //       },
-  //       {
-  //         "id": 2,
-  //         "name": "On Motion Live",
-  //         "price": 99,
-  //         "image": "https://s3.amazonaws.com/makeitreal/projects/e-commerce/camiseta-2.jpg"
-  //       },
-  //       {
-  //         "id": 3,
-  //         "name": "Underground Max",
-  //         "price": 149,
-  //         "image": "https://s3.amazonaws.com/makeitreal/projects/e-commerce/camiseta-3.jpg"
-  //       }
-  //     ]
-  //   }
-  // )
-};
 const addToCart = product => {
   return {
-    type: "ADD_TO_CART",
+    type: 'ADD_TO_CART',
     product
   }
-};
+}
 
 const removeFromCart = product => {
   return {
     type: 'REMODE_FROM_CAR',
     product
   }
-};
+}
 
-
-
-export { addToCart, removeFromCart, loadProducts}
+export { addToCart, removeFromCart, loadProducts }
--- src/store.js
+++ src/store.js
@@ -1,32 +1,16 @@
-import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux'
 import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import { cart, products } from './reducers'
 
-const products = ( state=[], action ) => {
-  switch (action.type) {
-    case 'REPLACE_PRODUCTS':
-      return action.products
-    default:
-  }
-  return state;
-};
-
-const cart = ( state=[], action ) => {
-  switch (action.type) {
-    case 'ADD_TO_CART':
-    console.log('ADD_TO_CART',state);
-      return state.concat(action.product).filter(element => element !== undefined)
-    case 'REMODE_FROM_CAR':
-    console.log('REMODE_FROM_CAR',state);
-      return state.filter(product => product.id !== action.product.id)
-    default:
-  }
-  return state;
-};
 const logger = store => next => action => {
-  let result;
+  let result
   result = next(action)
   console.log('next state', store.getState())
   return result
 }
 
-export default createStore(combineReducers({ cart, products}), applyMiddleware(logger, thunk));
+export default createStore(
+  combineReducers({ cart, products }),
+  composeWithDevTools(applyMiddleware(logger, thunk))
+)
